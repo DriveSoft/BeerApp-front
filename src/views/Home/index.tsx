58e3383d3818ef6bebf5eda880bdd72336a8195c
DriveSoft <support@drive-software.com>
2024-01-25 22:24:46 +0200

    Add Beer list filtering

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useState, useMemo } from 'react';
 import { fetchData } from './utils';
 import { Beer } from '../../types';
 import { Link as RouterLink } from 'react-router-dom';
@@ -9,10 +9,13 @@ import styles from './Home.module.css';
 const Home = () => {
 	const [beerList, setBeerList] = useState<Array<Beer>>([]);
 	const [savedList, setSavedList] = useState<Array<Beer>>([]);
+	const [filterValue, setFilterValue] = useState('');
 
 	// eslint-disable-next-line
 	useEffect(fetchData.bind(this, setBeerList), []);
 
+	const filteredBeerList = beerList.filter(beer => beer.name.toLowerCase().includes(filterValue.toLowerCase()));
+		
 	const onCheckBeer = (checked: boolean, id: string) => {
 		const newBeerState = beerList.map(beer => (beer.id === id ? {...beer, checked: checked} : beer));
 		setBeerList(newBeerState)
@@ -34,20 +37,20 @@ const Home = () => {
 					<Paper>
 						<div className={styles.listContainer}>
 							<div className={styles.listHeader}>
-								<TextField label='Filter...' variant='outlined' />
-								<Button variant='contained'>Reload list</Button>
+								<TextField label='Filter...' variant='outlined' onChange={(e: ChangeEvent<HTMLInputElement>) => setFilterValue(e.target.value) } />
+								<Button variant='contained' onClick={() => fetchData.bind(this, setBeerList)()}>Reload list</Button>
 							</div>
 							<ul className={styles.list}>
-								{beerList.map((beer, index) => (
+								{filteredBeerList.map((beer, index) => (
 									<li key={index.toString()}>
-										<Checkbox checked={beer.checked} onChange={(e, checked) => onCheckBeer(checked, beer.id)} />
+										<Checkbox checked={beer.checked} onChange={(_, checked) => onCheckBeer(checked, beer.id)} />
 										<Link component={RouterLink} to={`/beer/${beer.id}`}>
 											{beer.name}
 										</Link>
 									</li>
 								))}
 							</ul>
-							<Button variant='contained' onClick={onClickSaveSelectedItems} disabled={!beerList.some(beer => beer.checked)}>Save selected items</Button>
+							<Button variant='contained' onClick={onClickSaveSelectedItems} disabled={!filteredBeerList.some(beer => beer.checked)}>Save selected items</Button>
 						</div>
 					</Paper>
 
